55402535e7c819b4273531b4cc3bfdf97cb97e8a
qianbin <cola.tin.com@gmail.com>
2019-06-17 12:41:19 +0800

    default thor api base url

--- src/index.ts
+++ src/index.ts
@@ -10,65 +10,62 @@ process.on('unhandledRejection', reason => {
     //console.error('unhandled promise rejection', reason)
 })
 
-const baseUrl = process.argv[2]
-if (baseUrl) {
-    const networks: { [index: string]: string } = {
-        '0x00000000851caf3cfdb6e899cf5958bfb1ac3413d346d43539627e6be7ec1b4a': 'Mainnet',
-        '0x000000000b2bce3c70bc649a02749e8687721b09ed2e15997f466536b20bb127': 'Testnet',
-        '0x00000000973ceb7f343a58b08f0693d6701a5fd354ff73d7058af3fba222aea4': 'Solo'
-    }
+const baseUrl = process.argv[2] || 'http://localhost:8669/'
 
-    console.log(`VeChain Connex Playground`);
-
-    (async () => {
-        try {
-            const driver = await DriverNodeJS.connect(baseUrl)
-            const connex = new Framework(driver)
-            console.log(`connex v${connex.version}`)
-
-            const network = networks[connex.thor.genesis.id] || 'Custom'
-            const prompter = {
-                get text() {
-                    const progressStr = '' + Math.floor(connex.thor.status.progress * 1000) / 10
-                    return `${network}(${progressStr}%)> `
-                }
-            }
-
-            const txHistory = [] as object[]
-            driver.txConfig.watcher = obj => {
-                txHistory.push(obj)
-            }
-
-            const server = REPL.start(prompter.text)
-            setupREPL(server, {
-                connex,
-                thor: connex.thor,
-                vendor: connex.vendor,
-                wallet: driver.wallet,
-                txConfig: {
-                    get expiration() { return driver.txConfig.expiration },
-                    set expiration(v) { driver.txConfig.expiration = v },
-                    get gasPriceCoef() { return driver.txConfig.gasPriceCoef },
-                    set gasPriceCoef(v) { driver.txConfig.gasPriceCoef = v }
-                },
-                txHistory,
-                fromWei,
-                toWei
-            })
-
-            const ticker = connex.thor.ticker()
-            for (; ;) {
-                server.setPrompt(prompter.text)
-                await ticker.next()
-            }
-        } catch (err) {
-            console.error(err)
-            process.exit(1)
-        }
-    })()
-} else {
-    console.log('Usage: connex <base-url>')
+const networks: { [index: string]: string } = {
+    '0x00000000851caf3cfdb6e899cf5958bfb1ac3413d346d43539627e6be7ec1b4a': 'Mainnet',
+    '0x000000000b2bce3c70bc649a02749e8687721b09ed2e15997f466536b20bb127': 'Testnet',
+    '0x00000000973ceb7f343a58b08f0693d6701a5fd354ff73d7058af3fba222aea4': 'Solo'
 }
+
+console.log(`VeChain Connex Playground @ ${baseUrl}`);
+
+(async () => {
+    try {
+        const driver = await DriverNodeJS.connect(baseUrl)
+        const connex = new Framework(driver)
+        console.log(`connex v${connex.version}`)
+
+        const network = networks[connex.thor.genesis.id] || 'Custom'
+        const prompter = {
+            get text() {
+                const progressStr = '' + Math.floor(connex.thor.status.progress * 1000) / 10
+                return `${network}(${progressStr}%)> `
+            }
+        }
+
+        const txHistory = [] as object[]
+        driver.txConfig.watcher = obj => {
+            txHistory.push(obj)
+        }
+
+        const server = REPL.start(prompter.text)
+        setupREPL(server, {
+            connex,
+            thor: connex.thor,
+            vendor: connex.vendor,
+            wallet: driver.wallet,
+            txConfig: {
+                get expiration() { return driver.txConfig.expiration },
+                set expiration(v) { driver.txConfig.expiration = v },
+                get gasPriceCoef() { return driver.txConfig.gasPriceCoef },
+                set gasPriceCoef(v) { driver.txConfig.gasPriceCoef = v }
+            },
+            txHistory,
+            fromWei,
+            toWei
+        })
+
+        const ticker = connex.thor.ticker()
+        for (; ;) {
+            server.setPrompt(prompter.text)
+            await ticker.next()
+        }
+    } catch (err) {
+        console.error(err)
+        process.exit(1)
+    }
+})()
 
 
 function setupREPL(server: REPL.REPLServer, obj: object) {
